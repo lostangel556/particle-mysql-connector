d47c7e56ac693b0c1413331732b24a60e558c4c2
lostangel556 <steven.foxton@googlemail.com>
2015-12-04 23:57:12 +0000

    Update my-sql-connector.ino

--- examples/my-sql-connector.ino
+++ examples/my-sql-connector.ino
@@ -1,16 +1,16 @@
 #include "mysql.h"
 #include "sha1.h"
 
-IPAddress server_addr(192, 168, 0, 20);
+IPAddress server_addr(127, 0, 0, 1); //change this to your mySQL server
 /* Setup for the Connector */
 Connector my_conn; // The Connector reference
 char user[] = "root";
-char password[] = "raspberry";
+char password[] = "secret";
 
 
 void setup() {
     
-    char INSERT_SQL[] =  "INSERT INTO PiStat.Data VALUES ('2015-12-04 22:49:50, 'BCEE7B73F5EB', '20.5', '48.6')";
+    char INSERT_SQL[] =  "INSERT INTO test.Data VALUES ('2015-12-04 22:49:50, 'BCEE7B73F5EB', '20.5', '48.6')";
     if (my_conn.mysql_connect(server_addr, 3306, user, password))
       {
         delay(500); // Allowing it time, to be sure it has opened the connection
